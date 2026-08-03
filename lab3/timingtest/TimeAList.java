20acf81e6dadef9ba5c65fdb2b929cfbbcbcbde6
Xiaobu <279435887+XIAOBuLAN@users.noreply.github.com>
2026-08-03 13:23:45 +0800

    finished timingtest part

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -23,5 +23,26 @@ public class TimeAList {
 
     public static void timeAListConstruction() {
         // TODO: YOUR CODE HERE
+        System.out.println("Constructing time AList...");
+
+        AList<Integer> Ns= new AList<>();
+        AList<Double> times= new AList<>();
+
+        for (int i = 1000; i <= 128000; i *= 2) {
+            Ns.addLast(i);
+        }
+
+        for (int i = 0; i < Ns.size(); i += 1) {
+            int n = Ns.get(i);
+            AList<Integer> opCounts= new AList<>();
+            Stopwatch sw = new Stopwatch();
+            for(int j = 0; j < n; j += 1) {
+                opCounts.addLast(114514);
+            }
+            double time = sw.elapsedTime();
+            times.addLast(time);
+        }
+
+        printTimingTable(Ns, times, Ns);
     }
 }
