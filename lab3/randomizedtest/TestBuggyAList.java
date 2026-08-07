1c395ab2583f984826f0a8e7a381b9bc70aed542
Xiaobu <279435887+XIAOBuLAN@users.noreply.github.com>
2026-08-07 11:27:00 +0800

    finished lab3

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -2,11 +2,40 @@ package randomizedtest;
 
 import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  * Created by hug.
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void randomizedtest() {
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> broken = new BuggyAList<>();
+
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                correct.addLast(randVal);
+                broken.addLast(randVal);
+            } else if (operationNumber == 1) {
+                // size
+                int size = correct.size();
+                int size2 = broken.size();
+            } else if (operationNumber == 2) {
+                if (correct.size() != 0) {
+                    correct.getLast();
+                    broken.getLast();
+                }
+            } else if (operationNumber == 3) {
+                if (correct.size() != 0) {
+                    correct.removeLast();
+                    broken.removeLast();
+                }
+            }
+        }
+    }
 }
