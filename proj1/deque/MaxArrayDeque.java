4f275b0f4c0bf5b65ec27b1e1b5f2924a694fd61
Xiaobu <279435887+XIAOBuLAN@users.noreply.github.com>
2026-08-09 12:01:23 +0800

    add MaxArrayDeque

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,4 +1,37 @@
 package deque;
 
-public class MaxArrayDeque {
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
+
+    private final Comparator<T> comparator;
+
+    public MaxArrayDeque(Comparator<T> c){
+        super();
+        this.comparator = c;
+    }
+
+    private T getMax(Comparator<T> c){
+        if (isEmpty()) {
+            return null;
+        }
+
+        T max = this.get(0);
+
+        for (int i = 1; i < this.size(); i++) {
+            T item = this.get(i);
+            if (c.compare(item, max) > 0) {
+                max = item;
+            }
+        }
+        return max;
+    }
+
+    public T max(){
+        return getMax(this.comparator);
+    }
+
+    public T max(Comparator<T> c){
+        return getMax(c);
+    }
 }
