667aeac6d3084c87a17036f0d5d8feddf549ee09
Xiaobu <279435887+XIAOBuLAN@users.noreply.github.com>
2026-08-19 13:56:37 +0800

    happy

--- lab4/flik/Filktest.java
+++ lab4/flik/Filktest.java
@@ -1,11 +1,16 @@
 package flik;
 
 import org.junit.Test;
+import edu.princeton.cs.algs4.StdRandom;
+
+import static org.junit.Assert.assertEquals;
 
 public class Filktest {
     @Test
-    public void test1() {
-        
+    public void test() {
+            boolean broken = Flik.isSameNumber(128, 128);
+            boolean expected = true;
+            assertEquals(expected, broken);
     }
 
 }
